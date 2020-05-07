207024cd35b70b2b94a3500c5d07553a70ffbff5
niroshan123 <niroshanratnayake07@gmail.com>
2020-05-08 00:17:36 +0530

    google authentication

--- Shop-With-Mee/src/app/bs-navbar/bs-navbar.component.ts
+++ Shop-With-Mee/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import *as firebase from 'firebase';
 
 @Component({
   selector: 'bs-navbar',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BsNavbarComponent implements OnInit {
 
-  constructor() { }
+user:firebase.User;
+
+  constructor(private afA:AngularFireAuth) { 
+
+  afA.authState.subscribe(user=>this.user=user);
+  afA.authState.subscribe(x=>console.log(x));
+  }
 
   ngOnInit() {
+  }
+  logout(){
+    this.afA.auth.signOut();
   }
 
 }
